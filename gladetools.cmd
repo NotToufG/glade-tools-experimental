0be40a0f29562d188ed3a72750f45f175a4d075b
Tuff Enjoyer <108702845+NotToufG@users.noreply.github.com>
2024-01-22 20:01:35 +0100

    Update gladetools.cmd

--- gladetools.cmd
+++ gladetools.cmd
@@ -1,1 +1,10 @@
-echo This is a test!
+@echo off
+if [%1]==[] (
+echo Welcome to GladeTools!             
+)
+
+if [%1]==[update] (
+echo Updating....
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/NotToufG/glade-tools-experimental/main/gladetools.cmd -OutFile gladetools.cmd"
+echo Done!
+)
